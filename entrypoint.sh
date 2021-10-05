667d12d39633490061c63ce7470deafe793665f4
Rennan Cockles <rennancockles@gmail.com>
2021-10-05 01:38:59 -0300

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh -l
 
-yarn build
+# yarn build
 
-cd dist
+pwd
 
-git init
-git config --global user.email r3ck.dev@gmail.com
-git config --global user.name "Rennan Cockles"
-git remote add origin https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
-git checkout -b gh-pages
-git add .
-git commit -m "Publishing Resume ($GITHUB_RUN_NUMBER)."
-git push origin gh-pages -f
+ls
+
+
+
+# cd dist
+
+# git init
+# git config --global user.email r3ck.dev@gmail.com
+# git config --global user.name "Rennan Cockles"
+# git remote add origin https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+# git checkout -b gh-pages
+# git add .
+# git commit -m "Publishing Resume ($GITHUB_RUN_NUMBER)."
+# git push origin gh-pages -f
