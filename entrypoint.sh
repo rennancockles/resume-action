e6fa2bd992c5954b306fca757d304a6f3944477e
Rennan Cockles <rennancockles@gmail.com>
2021-10-05 10:22:07 -0300

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,28 @@ cd /app
 
 yarn build
 
-cd dist
+cd /github/workspace
 
-git init
-git config --global user.email r3ck.dev@gmail.com
-git config --global user.name "Rennan Cockles"
-git remote add origin https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
-git checkout -b gh-pages
+git checkout --orphan gh-pages
+cp -r /app/dist/* ./
+
+if [ $INPUT_CNAME ] ; then
+  echo "Writing CNAME to file: $INPUT_CNAME"
+  echo $INPUT_CNAME > CNAME
+else
+  echo "Skipping CNAME not provided"
+fi
+
 git add .
 git commit -m "Publishing Resume ($GITHUB_RUN_NUMBER)."
 git push origin gh-pages -f
+
+
+# git init
+# git config --global user.email r3ck.dev@gmail.com
+# git config --global user.name "Rennan Cockles"
+# git remote add origin https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+# git checkout -b gh-pages
+# git add .
+# git commit -m "Publishing Resume ($GITHUB_RUN_NUMBER)."
+# git push origin gh-pages -f
