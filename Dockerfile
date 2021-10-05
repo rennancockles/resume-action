3dffe1ecc5c5aa4257b9a5580e7fb1ae59caad23
Rennan Cockles <rennancockles@gmail.com>
2021-10-05 13:10:17 -0300

    update entrypoint

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,7 @@ FROM rennancockles/resume-template:latest
 
 LABEL maintainer="Rennan Cockles <r3ck.dev@gmail.com>"
 
-RUN apk update && apk add git
-
 WORKDIR /app
-
-# COPY ${INPUT_DIRECTORY} ./src/data/
 
 COPY entrypoint.sh .
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -15,35 +15,19 @@ cd /github/workspace
 
 git config --global user.email r3ck.dev@gmail.com
 git config --global user.name "Rennan Cockles"
+
 git checkout --orphan gh-pages
 git rm -rf .
-# rm -rf ./*
 
-echo '------------|||---------------'
-ls
-echo '------------------------------'
-git status -s
-echo '------------|||---------------'
+cp -r /app/dist/* ./
 
-# cp -r /app/dist/* ./
+if [ $INPUT_CNAME ] ; then
+  echo "Writing CNAME to file: $INPUT_CNAME"
+  echo $INPUT_CNAME > CNAME
+else
+  echo "Skipping CNAME not provided"
+fi
 
-# if [ $INPUT_CNAME ] ; then
-#   echo "Writing CNAME to file: $INPUT_CNAME"
-#   echo $INPUT_CNAME > CNAME
-# else
-#   echo "Skipping CNAME not provided"
-# fi
-
-# git add .
-# git commit -m "Publishing Resume ($GITHUB_RUN_NUMBER)."
-# git push origin gh-pages -f
-
-
-# git init
-# git config --global user.email r3ck.dev@gmail.com
-# git config --global user.name "Rennan Cockles"
-# git remote add origin https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
-# git checkout -b gh-pages
-# git add .
-# git commit -m "Publishing Resume ($GITHUB_RUN_NUMBER)."
-# git push origin gh-pages -f
+git add .
+git commit -m "Publishing Resume ($GITHUB_RUN_NUMBER)."
+git push origin gh-pages -f
